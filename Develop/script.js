257ded3eaadff6b73fd90f1829f3dc55f67ad81f
GunnySensei <gunnerwagoner@hotmail.com>
2021-11-21 11:25:24 -0600

    added prompts for user input

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// generate password based on rules
+
+function generatePassword () {
+  var length = window.prompt('Enter the length of your password between 8 and 128 characters.');
+  console.log(length);
+  var lowercase = window.prompt('Would you like to include lowercase characters? [Enter: YES or NO]');
+  console.log(lowercase);
+  var uppercase = window.prompt('Would you like to include uppercase characters? [Enter: YES or NO]');
+  console.log(uppercase);
+  var numeric = window.prompt('Would you like to include numeric characters? [Enter: YES or NO]');
+  console.log(numeric);
+  var specialChar = window.prompt('Would you like to include special characters? [Enter: YES or NO]');
+  console.log(specialChar);
+
+}
+
+var lengthCheck = function(length) {
+}
